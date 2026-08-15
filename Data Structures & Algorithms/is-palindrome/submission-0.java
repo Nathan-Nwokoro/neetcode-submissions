fccff2fced139518a73a4a922b170687c0234f01
Nathan Nwokoro <nathannwokoro@icloud.com>
2026-08-15 23:01:15 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int first = 0;
+        int last = s.length() - 1;
+        while (first < last) {
+            if (s.charAt(first) == s.charAt(last)) {
+                first ++;
+                last --;
+            } else return false;
+        }
+        return true;
+    }
+}
